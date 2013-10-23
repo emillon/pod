c95503afd63b8b50b8086e88a15445d6c661cf09
Etienne Millon <me@emillon.org>
2013-10-23 19:08:14 +0200

    csrf token & key

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,26 @@
+import os
+from random import choice
+from string import ascii_uppercase, digits
 from flask import Flask
 
+def generate_key(n):
+    return ''.join(choice(ascii_uppercase + digits) for x in range(n))
+
+
+def get_secret_key(app):
+    filename = os.path.join(app.instance_path, 'secret.key')
+    try:
+        with open(filename) as f:
+            secret = f.read()
+    except IOError as e:
+        secret = generate_key(64)
+        with open(filename, 'w+') as f:
+            f.write(secret)
+    return secret
+
+
 app = Flask(__name__)
+app.config.from_object('config')
+app.config['SECRET_KEY'] = get_secret_key(app)
 
 from app import views
