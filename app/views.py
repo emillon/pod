7904b3f9ca7aec6f8237851e53eb23b91e02898c
Etienne Millon <me@emillon.org>
2013-10-23 18:47:20 +0200

    new feed page

--- app/views.py
+++ app/views.py
@@ -3,4 +3,8 @@ from app import app
 
 @app.route('/')
 def home():
-    return render_template('home.html', content='Hello')
+    return render_template('home.html')
+
+@app.route('/new')
+def new_feed():
+    return render_template('new_feed.html')
