a4b5c0f1cc371a51ae3c7b12ad1ff7a41aba6237
Etienne Millon <me@emillon.org>
2013-10-24 15:23:10 +0200

    form redirect

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from forms import NewFeedForm
 
@@ -6,7 +6,10 @@ from forms import NewFeedForm
 def home():
     return render_template('home.html')
 
-@app.route('/new')
+@app.route('/new', methods=['GET', 'POST'])
 def new_feed():
     form = NewFeedForm()
+    if form.validate_on_submit():
+        flash('Adding podcast : ' + form.podcast_url.data)
+        return redirect('/')
     return render_template('new_feed.html', title='New feed', form=form)
