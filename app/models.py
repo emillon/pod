940b0eb150a0265b7730fd01ac98ca8a7d6e9002
Etienne Millon <me@emillon.org>
2013-10-29 17:17:40 +0100

    model doc

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+"""
+Models
+
+When editing this file don't forget to set up a migration in alembic.
+
+Coding rules:
+  - class names are singular
+  - be explicit about nullable
+"""
+
 from app import db
 import bcrypt
 
@@ -7,6 +17,9 @@ ROLE_ADMIN = 1
 
 
 class User(db.Model):
+    """
+    Application user. Someone that can log in.
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
@@ -18,37 +31,62 @@ class User(db.Model):
         self.password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     def is_active(self):
+        """
+        Needed for flask-login.
+        """
         return True
 
     def is_anonymous(self):
+        """
+        Needed for flask-login.
+        """
         return False
 
     def is_authenticated(self):
+        """
+        Needed for flask-login.
+        """
         return True
 
     def get_id(self):
+        """
+        Needed for flask-login.
+        """
         return unicode(self.id)
 
     def is_admin(self):
+        """
+        Has the user got administrative rights?
+        This grants access to admin panel, so careful.
+        """
         return (self.role == ROLE_ADMIN)
 
 
 class Feed(db.Model):
+    """
+    A RSS Feed.
+    When adding one, subsequent episodes must be fetched through tasks.get_feed.
+    """
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String, unique=True, nullable=False)
     title = db.Column(db.String, nullable=False)
 
-    def __init__(self, url, fp):
+    def __init__(self, url, feed):
         """
         Parameters
 
-        fp: a feedparser object
+        feed: a feedparser object
         """
         self.url = url
-        self.title = fp['feed']['title']
+        self.title = feed['feed']['title']
 
 
 class Episode(db.Model):
+    """
+    A podcast episode.
+
+    It can has an enclosure (audio file) or not.
+    """
     id = db.Column(db.Integer, primary_key=True)
     feed = db.Column(db.Integer, db.ForeignKey('feed.id'), nullable=False)
     title = db.Column(db.String, nullable=False)
