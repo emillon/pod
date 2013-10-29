a7ae3bb3f080564aaebe49b86ae8613377b774cf
Etienne Millon <me@emillon.org>
2013-10-29 17:22:47 +0100

    document tasks

--- app/tasks.py
+++ app/tasks.py
@@ -1,29 +1,39 @@
+"""
+Tasks ran asynchronously through RQ
+"""
+
 import feedparser
-from models import Feed, Episode
+from app.models import Feed, Episode
 from app import db
-from utils import job
+from app.utils import job
 
 
 def find_enclosure(entry):
     """
-    entry is a feedparser entry (one element of fp['entries'])
+    Find first enclosure in a feedparser entry
+    (one element of fp['entries'])
     """
     if 'links' in entry:
-        for l in entry['links']:
-            if l['rel'] == 'enclosure':
-                return l['href']
+        for link in entry['links']:
+            if link['rel'] == 'enclosure':
+                return link['href']
     return None
 
 
 @job
 def get_feed(url):
-    fp = feedparser.parse(url)
-    f = Feed(url, fp)
-    db.session.add(f)
+    """
+      - download a RSS url
+      - make a Feed entry for it
+      - populate corresponding Episodes
+    """
+    feedp = feedparser.parse(url)
+    feed = Feed(url, feedp)
+    db.session.add(feed)
     db.session.flush()
-    for e in fp['entries']:
-        title = e['title']
-        enclosure = find_enclosure(e)
-        ep = Episode(f.id, title, enclosure)
-        db.session.add(ep)
+    for entry in feedp['entries']:
+        title = entry['title']
+        enclosure = find_enclosure(entry)
+        episode = Episode(feed.id, title, enclosure)
+        db.session.add(episode)
     db.session.commit()
